a536c275ed931cdaa36d01762e03e7a543a10126
shaud93 <85545994+shaud93@users.noreply.github.com>
2022-04-30 21:27:25 -0400

    started microservice

---
+++ quintrix-spring-client/src/main/java/com/quintrix/jfs/quintrixspringclient/quintrixspringclient/QuintrixSpringClientApplication.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixspringclient.quintrixspringclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuintrixSpringClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuintrixSpringClientApplication.class, args);
+	}
+
+}
---
+++ quintrix-spring-client/src/test/java/com/quintrix/jfs/quintrixspringclient/quintrixspringclient/QuintrixSpringClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quintrix.jfs.quintrixspringclient.quintrixspringclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuintrixSpringClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
